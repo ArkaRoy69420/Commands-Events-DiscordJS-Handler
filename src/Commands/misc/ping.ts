9139387c403f47e0780373cf8df802442922ef9e
Arka Roy <arka.ry2010@gmail.com>
2024-03-06 15:41:21 +0530

    Minor fixes

--- src/Commands/misc/ping.ts
+++ src/Commands/misc/ping.ts
@@ -1,14 +1,45 @@
-import { Client, CommandInteraction } from "discord.js";
+import { Client, CommandInteraction, EmbedBuilder } from "discord.js";
 import { CommandObject } from "../../Utils/CommandObjectClass";
+import "colors";
 
 export = new CommandObject({
-    name: "ping",
-    description: "Pings the bot and returns it's latency UwU",
-    callback: async (client: Client, interaction: CommandInteraction) => {
-        await interaction.reply("pong");
-        // add the logic you want.
-    },
-    forDevOnly: true,
-    deleted: true, 
-    // rest of the code
-})
+  name: "ping",
+  description: "Pings the bot and returns it's latency UwU",
+  callback: async (client: Client, interaction: CommandInteraction) => {
+    await interaction.deferReply();
+
+    try {
+      const reply = await interaction.fetchReply();
+      const latency: number =
+        reply.createdTimestamp - interaction.createdTimestamp;
+
+      await interaction.editReply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("Pong! ✍️(◔◡◔)")
+            .setDescription("With latencies:")
+            .addFields(
+              {
+                name: "Client ping:",
+                value: `${latency}ms`,
+              },
+              {
+                name: "Websocket ping",
+                value: `${client.ws.ping}ms`,
+              }
+            )
+            .setColor("Aqua"),
+        ],
+      });
+    } catch (error) {
+      console.error(
+        `${"[ERROR]".red} Error while running the command ${
+          interaction.commandName
+        }: \n ${error}`
+      );
+    }
+  },
+  forDevOnly: true,
+  deleted: false,
+  // rest of the code
+});
